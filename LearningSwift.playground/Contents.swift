6c2201293702706ea752e6a0c92f101af3939618
shashank ch <shashank.ch@shop101.com>
2016-06-10 10:01:43 +0530

    Adding playground files to the repo.

---
+++ LearningSwift.playground/Contents.swift
@@ -0,0 +1,56 @@
+//: Playground - noun: a place where people can play
+
+var someVar: Int
+
+someVar = 56
+print(someVar)
+
+let variable = 5
+
+
+for case let i in i...5 where i < 5 {
+    print("loop excuted")
+}
+
+
+
+//class MyClassToTestReference {
+//
+//    var property1 = "this is property 1"
+//
+//}
+//
+//var object1 = MyClassToTestReference()
+//
+//var obj2 = MyClassToTestReference()
+//
+//print(object1.property1)
+//print(obj2.property1)
+//
+//obj2 = object1
+//
+//object1.property1 = "change this"
+//
+//print(object1.property1)
+//print(obj2.property1)
+//
+//object1 = MyClassToTestReference()
+//
+//
+//print(object1.property1)
+//print(obj2.property1)
+//
+//var value1 = 23
+//var value2 = value1
+//
+//print(value1, value2)
+//
+//value1 = 50
+//print(value1, value2)
+//
+//
+//
+
+
+
+
---
+++ MyPlayground12.playground/Contents.swift
@@ -0,0 +1,63 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+class Man {
+    var name: String?
+}
+
+class Boy {
+    var boyName: String?
+}
+
+
+let man1 = Man()
+man1.name = "king"
+
+
+
+
+var array1 = [man1]
+
+var array2 = [man1, man1]
+
+
+
+man1.name = "fight"
+
+array1
+array2
+
+
+let man2 = Man()
+man2.name = "233"
+
+print(man2.name)
+
+
+let a1 = [1, 2, 3]
+let a2 = [2, 3, 1]
+
+// Check if they are the same
+
+if a1==a2 {
+    print("S1 is the same as S2")
+}
+else {
+    print("S1 is not the same as S2")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
